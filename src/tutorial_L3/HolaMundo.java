7cdfa5300b48a2ffd1c82ac43882ded0847cef81
carmenperezgomez <carmenperezgomez@localhost>
2024-09-23 17:36:54 +0200

    Cambio tutorial

--- src/tutorial_L3/HolaMundo.java
+++ src/tutorial_L3/HolaMundo.java
@@ -2,4 +2,9 @@ package tutorial_L3;
 
 public class HolaMundo {
 
+	public static void main(String[] arg){
+	
+		System.out.println("Hola CMEPPS");
+		System.out.println("Bienvenidos a todos ");
+	}
 }
